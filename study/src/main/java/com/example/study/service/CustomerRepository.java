fadea89fb4dd21470abd9f815a86a63b65232bdf
quan.th <tranhongquanutcit3@gmail.com>
2020-06-03 18:20:44 +0700

    change build.gradle

--- study/src/main/java/com/example/study/service/CustomerRepository.java
+++ study/src/main/java/com/example/study/service/CustomerRepository.java
@@ -1,31 +1,29 @@
 package com.example.study.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Slf4j
 public class CustomerRepository {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+//    @Autowired
+//    @Qualifier(value = "sessionFactory")
+//    private SessionFactory sessionFactory;
 
     public void create(Customer customer){
-        try {
-            Session session = this.sessionFactory.getCurrentSession();
-            session.save(customer);
-        }catch (HibernateException e){
-            log.error("save failed!");
-            throw e;
-        }
+//        try {
+//            Session session = this.sessionFactory.getCurrentSession();
+//            session.save(customer);
+//        }catch (HibernateException e){
+//            log.error("save failed!");
+//            throw e;
+//        }
     }
 
     public Customer findById(String id) {
-        Session session = this.sessionFactory.getCurrentSession();
-        return session.get(Customer.class, id);
+//        Session session = this.sessionFactory.getCurrentSession();
+//        return session.get(Customer.class, id);
+        return null;
     }
 }
--- study/src/main/java/com/example/study/service/CustomerService.java
+++ study/src/main/java/com/example/study/service/CustomerService.java
@@ -3,8 +3,8 @@ package com.example.study.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import javax.validation.Validator;
 
 @Service
